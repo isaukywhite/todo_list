d28257ea54237a07436a2b36afa97df71503f923
Isac Martins <50426537+isaukywhite@users.noreply.github.com>
2022-07-15 23:31:41 -0300

    mudança da store para singleton

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import '../../modules/speech/speech.dart';
 import '../../modules/todo_database/models/todo.dart';
 import '../../modules/todo_database/todo_database.dart';
+import 'components/card_todo.dart';
+import 'components/dialog_alert.dart';
 import 'home_store.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,7 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeStore store = HomeStore();
+  final HomeStore store = HomeStore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -22,74 +24,22 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(
             child: ValueListenableBuilder<List<Todo>>(
-                valueListenable: store.todos,
-                builder: (context, value, _) {
-                  if (value.isEmpty) {
-                    return const Center(
-                      child: Text("You don't have any todos yet"),
-                    );
-                  }
-                  return ListView.builder(
-                      itemCount: value.length,
-                      itemBuilder: (ctx, index) {
-                        final todo = value[index];
-                        return Card(
-                          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: GestureDetector(
-                            child: CheckboxListTile(
-                              title: Text(
-                                todo.title,
-                                style: TextStyle(
-                                  fontSize: 19,
-                                  color: Colors.green[900],
-                                ),
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
-                              value: todo.completed,
-                              checkColor: Colors.green[900],
-                              activeColor: Colors.greenAccent,
-                              onChanged: (bool? value) {
-                                if (value == null) return;
-                                todo.completed = value;
-                                TodoDatabaseImpl.i.saveTodo(todo);
-                                store.loadTodos();
-                              },
-                            ),
-                            onLongPress: () {
-                              showDialog(
-                                context: context,
-                                builder: (_) => AlertDialog(
-                                  title: const Text('Confirmar'),
-                                  content: const Text('Deseja excluir o todo?'),
-                                  actions: [
-                                    TextButton(
-                                      child: const Text('Sim'),
-                                      onPressed: () {
-                                        TodoDatabaseImpl.i
-                                            .deleteTodo(todo)
-                                            .then((value) {
-                                          store.loadTodos();
-                                        });
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                    TextButton(
-                                      child: const Text('Não'),
-                                      onPressed: () => Navigator.pop(context),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                          ),
-                        );
-                      });
-                }),
+              valueListenable: store.todos,
+              builder: (context, value, _) {
+                if (value.isEmpty) {
+                  return const Center(
+                    child: Text("You don't have any todos yet"),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (ctx, index) {
+                    final todo = value[index];
+                    return CardTodo(todo: todo);
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -105,46 +55,29 @@ class _HomePageState extends State<HomePage> {
             ),
             onPressed: () async {
               store.recording.value = true;
-              final resp = await SpeechCustom.getText();
-              store.recording.value = false;
-              if (resp.isEmpty) {
-                showDialog(
-                  context: context,
-                  builder: (ctx) => AlertDialog(
-                    title: const Text('Erro'),
-                    content: const Text('Nenhuma palavra foi reconhecida.'),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(ctx),
-                        child: const Text('Ok'),
-                      ),
-                    ],
-                  ),
-                );
-                return;
-              }
-              await TodoDatabaseImpl.i.saveTodo(
-                Todo(
-                  title: resp,
-                  completed: false,
-                ),
-              );
-              showDialog(
-                context: context,
-                builder: (ctx) {
-                  return AlertDialog(
-                    title: const Text('Sucesso'),
-                    content: const Text('Tarefa adicionada com sucesso.'),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(ctx),
-                        child: const Text('Ok'),
-                      ),
-                    ],
+              await SpeechCustom.getText().then(
+                (resp) {
+                  store.recording.value = false;
+                  if (resp.isEmpty) {
+                    const DialogAlert(
+                      title: 'Erro',
+                      content: 'Nenhuma palavra foi reconhecida',
+                    ).show(context);
+                    return;
+                  }
+                  TodoDatabaseImpl.i
+                      .saveTodo(Todo(title: resp, completed: false))
+                      .then(
+                    (_) {
+                      const DialogAlert(
+                        title: 'Sucesso',
+                        content: 'Todo adicionado com sucesso',
+                      ).show(context);
+                    },
                   );
+                  store.loadTodos();
                 },
               );
-              store.loadTodos();
             },
           );
         },
